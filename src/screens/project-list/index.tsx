8751a06e96c4ee1c5c14ce0dbf3324a2f6637bff
niuchen <niuchen@zto.com>
2021-10-11 17:27:37 +0800

    fix: add hook and debounce

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -3,7 +3,7 @@ import { List } from "./list";
 import { SearchPanel } from "./search-panel";
 import { useEffect, useState } from "react";
 import { productList } from "api/list";
-import { clearObject } from "utils/until";
+import { clearObject, useMount, useDebounce } from "utils/until";
 import qs from "qs";
 export const ProjectListScreen = () => {
   /* 声明一个响应式变量param,并声明一个更新param的函数，我们命名为setParam */
@@ -16,16 +16,18 @@ export const ProjectListScreen = () => {
   const [users, setUsers]: [any, Function] = useState([]);
   /* 列表信息 */
   const [list, setList] = useState([]);
+  const debounceVal = useDebounce(param, 1000);
   useEffect(() => {
     productList.getList(qs.stringify(clearObject(param))).then((res: any) => {
       setList(res);
     });
-  }, [param]);
-  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debounceVal]);
+  useMount(() => {
     productList.getUser().then((res) => {
       setUsers(res);
     });
-  }, []);
+  });
   return (
     <div>
       <SearchPanel
--- src/utils/until.ts
+++ src/utils/until.ts
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export function isFalsy(value: any) {
   return value === 0 ? true : !!value;
 }
@@ -10,3 +12,24 @@ export function clearObject(object: any) {
   });
   return result;
 }
+
+// 自定义 Hook 是一个函数，其名称以 “use” 开头，函数内部可以调用其他的 Hook
+export const useMount = (callBack: Function) => {
+  useEffect(() => {
+    callBack();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+// 这是一个关于防抖的功能
+export const useDebounce = (value: any, delay: number) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+  useEffect(() => {
+    // 每次改变都创建一个定时器
+    let timeout = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay);
+    // 在上一个 useEffect处理完成后执行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debounceValue;
+};
